4b18a26e561edcabb8a37f330c9d100f61baabff
PedroMakengo <pedromakengomiguel@gmail.com>
2025-10-21 09:44:46 +0100

    Adicionando a parte de trazer os dados

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-purple-600">
+    <h1>Código ao Ponto</h1>
+
+    {{ data }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { data, status, error, refresh, clear } = await useFetch(
+  "https://rickandmortyapi.com/api/character"
+);
+
+console.log(data.value);
+</script>
--- middleware/01.removeTrailingSlash.global.ts
+++ middleware/01.removeTrailingSlash.global.ts
@@ -3,12 +3,6 @@ import { defineNuxtRouteMiddleware, navigateTo } from "nuxt/app";
 export default defineNuxtRouteMiddleware((to, from) => {
   const hasTrailingSlash = to.path !== "/" && to.path.endsWith("/");
 
-  console.log("to");
-  console.log(to);
-
-  console.log("from");
-  console.log(from);
-
   if (hasTrailingSlash) {
     const newPath = to.path.slice(0, -1);
 
