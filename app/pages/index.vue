1b80750bf357b49d87467251a915960155f5646c
PedroMakengo <pedromakengomiguel@gmail.com>
2025-10-21 11:49:58 +0100

    Adicionando a estrutura do projeto

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="text-purple-600">
-    <h1>Código ao Ponto</h1>
-
-    {{ data }}
+  <div class="text-white">
+    <div class="flex flex-wrap w-full max-w-[1224px] gap-4 mx-auto p-4">
+      <div
+        v-for="(currentCharacter, index) in data.results"
+        :key="index"
+        class="bg-[#313234] rounded-lg"
+      >
+        <img :src="currentCharacter.image" height="200" width="262" />
+        <div class="grid grid-cols-[1fr, 48px]">
+          <p>{{ currentCharacter.name }}</p>
+          <p>{{ currentCharacter.status }}</p>
+          <p>{{ currentCharacter.species }}</p>
+          <p>{{ currentCharacter.origin.name }}</p>
+          <span></span>
+        </div>
+        <NuxtLink :to="currentCharacter.url">Saiba Mais</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
